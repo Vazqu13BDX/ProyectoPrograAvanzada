ac75d43083203799d0df280fa84f873612cd11a7
Vazqu13BDX <155490298+Vazqu13BDX@users.noreply.github.com>
2024-01-02 17:44:45 -0600

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,79 @@
 #include "mainwindow.h"
+#include <QtWidgets>
+#include <QLabel>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+
+    tab_bar = new QTabBar(this);
+        tab_bar->addTab("Proceso de Llenado");
+        tab_bar->addTab("Almacen");
+        tab_bar->addTab("Envios y Rastreo");
+        connect(tab_bar, SIGNAL(currentChanged(int)), this, SLOT(cambiarPestana(int)));
+
+    // Crear los contenidos de las pestañas
+    contenidoPestana1 = new QWidget(this);
+        labelPestana1 = new QLabel("Proceso de llenado", contenidoPestana1);
+        layoutPestana1 = new QVBoxLayout(contenidoPestana1);
+        layoutPestana1->addWidget(labelPestana1);
+
+    contenidoPestana2 = new QWidget(this);
+        labelPestana2 = new QLabel("Almacen de Embotelladora", contenidoPestana2);
+        layoutPestana2 = new QVBoxLayout(contenidoPestana2);
+        layoutPestana2->addWidget(labelPestana2);
+
+    contenidoPestana3= new QWidget(this);
+        labelPestana3= new QLabel("Envios y Rastreo");
+        layoutPestana3= new QVBoxLayout(contenidoPestana3);
+        layoutPestana3->addWidget(labelPestana3);
+
+
+    // Crear el diseño principal
+    layoutPrincipal = new QVBoxLayout();
+    layoutPrincipal->addWidget(tab_bar);
+    layoutPrincipal->addWidget(contenidoPestana1);
+    layoutPrincipal->addWidget(contenidoPestana2);
+    layoutPrincipal->addWidget(contenidoPestana3);
+
+    // Crear el widget central y establecer el diseño principal
+
+    centralWidget = new QWidget(this);
+    centralWidget->setLayout(layoutPrincipal);
+    setCentralWidget(centralWidget);
+
+
+
+
+
+
+    setWindowTitle("Embotelladora Industrial");
+    setMinimumSize(200,200);
+    resize(480,320);
 }
 
 MainWindow::~MainWindow()
 {
+
+}
+
+void MainWindow::cambiarPestana(int index)
+{
+        if (index == 0) {
+            labelPestana1->setText("Contenido 1");
+            labelPestana2->setText("");
+            labelPestana3->setText(""); // Limpiar contenido de la Pestaña
+        }
+        else if (index == 1) {
+            labelPestana1->setText("");  // Limpiar contenido de la Pestaña 1
+            labelPestana2->setText("Contenido 2");
+            labelPestana3->setText("");
+        }
+        else if (index==2){
+            labelPestana1->setText("");
+            labelPestana2->setText("");
+            labelPestana3->setText("Contenido 3");
+        }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,40 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets>
+
+class QAction;
+class QTabBar;
+class QLabel;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private:
+    QTabBar *tab_bar;
+
+    QWidget *contenidoPestana1;
+    QLabel *labelPestana1;
+    QVBoxLayout *layoutPestana1;
+
+    QWidget *contenidoPestana2;
+    QLabel *labelPestana2;
+    QVBoxLayout *layoutPestana2;
+
+    QWidget *contenidoPestana3;
+    QLabel *labelPestana3;
+    QVBoxLayout *layoutPestana3;
+
+    QVBoxLayout *layoutPrincipal;
+    QWidget *centralWidget;
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void cambiarPestana(int index);
+private slots:
+
 };
 #endif // MAINWINDOW_H
